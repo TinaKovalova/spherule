18ef814f41088e15fe392966a80e74c2e83186b4
TinaKovalova <kovalevatkv@gmail.com>
2025-04-26 00:29:49 +0300

    header

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const burgerButton = document.querySelector(".burger");
+const body = document.querySelector("body");
+
+burgerButton.addEventListener("click", () => {
+    body.classList.toggle("_active");
+})
