6d59325273c0e364137d93ef112394f714207674
TinaKovalova <kovalevatkv@gmail.com>
2025-05-02 23:10:11 +0300

    sroll, button-up

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
+'use strict';
 const burgerButton = document.querySelector(".burger");
-const body = document.querySelector("body");
+const body = document.body;
+const navigation = document.querySelector(".header__nav");
+const buttonUp = document.querySelector(".button-up");
+
 
 burgerButton.addEventListener("click", () => {
     body.classList.toggle("_active");
+});
+
+navigation.addEventListener("click", (e) => {
+    if (e.target.closest(".navigation__link") && body.classList.contains("_active")) {
+        body.classList.remove("_active");
+    }
+})
+
+window.addEventListener("scroll", () => {
+   buttonUp.classList.toggle("visible", document.documentElement.scrollTop > 200);
+});
+buttonUp.addEventListener("click", () => {
+  document.documentElement.scrollIntoView({
+    behavior: "smooth",
+  });
 });
